5c2ec2590b07d7f3c4159c97c6c6d8ed3c1973c9
pumpkin-ws <pumpkin_wang@foxmail.com>
2021-11-10 18:19:37 +0800

    this library provides general abstraction of ros

---
+++ build/CMakeFiles/ros-general-template.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ros-general-template.dir/main.cpp.o"
+  "ros-general-template"
+  "ros-general-template.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ros-general-template.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ws/spark-projects/ros_messaging_abstraction
+# Build directory: /home/ws/spark-projects/ros_messaging_abstraction/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
